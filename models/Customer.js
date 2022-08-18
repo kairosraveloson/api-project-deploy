2eb1eb687bead74adb10d5da6314f9a7880e2bdf
kairosraveloson <110980868+kairosraveloson@users.noreply.github.com>
2022-08-18 09:43:50 +0100

    Update Customer.js

--- models/Customer.js
+++ models/Customer.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  customerID: {
+  customerid: {
     type: String,
     required: true,
   },
