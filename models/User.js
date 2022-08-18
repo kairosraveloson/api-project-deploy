eae1a02dbed63383aca81ff9149adc30972e8833
kairosraveloson <110980868+kairosraveloson@users.noreply.github.com>
2022-08-18 09:44:40 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({ //Database name
-  userID: {
+  userid: {
     type: String,
     required: true,
   },
