65428cab144c09e0acd09a38b3d00f79af517771
kairosraveloson <110980868+kairosraveloson@users.noreply.github.com>
2022-08-29 11:28:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ const express = require('express'); // Spécification du package à utiliser
 const app = express(); // Définition de l'application, qui est une convention
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+var cors = require('cors');
 require("dotenv/config");
 
 require('./models/dbConfig');
 const postsRoutes = require('./routes/postsController');
 const bodyParser = require('body-parser');
 
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:4000'];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          'The CORS policy for this site does not ' +
+          'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 app.use(bodyParser.json());
 app.use('/',postsRoutes); // Ce middleware surveille que cette fonction se déclenche seulement quand on est sur la racine + le chemin donné
