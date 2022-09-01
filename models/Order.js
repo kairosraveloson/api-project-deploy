e4abc78c648fc624a9e305bd948a2ec0afa63851
kairosraveloson <110980868+kairosraveloson@users.noreply.github.com>
2022-09-01 07:41:09 +0100

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const storeSchema = new Schema({
@@ -7,9 +7,7 @@ const storeSchema = new Schema({
   User_ID: String,
   User_name: String,
   User_phone: String,
-  Date_creation: String,
   commentaire_order: String,
-  Date_creation: String,
   Current_state: String,
   is_delivery: Boolean,
   delivery_information: {
@@ -30,7 +28,30 @@ const storeSchema = new Schema({
     table_size: String,
     Table_reservation_cost: String,
   },
+  item_informations: [
+    {
+      item_id: String,
+      item_description: String,
+      item_type: String,
+      item_url: String,
+      item_quantity: String,
+      item_price_ht: String,
+      item_price_ttc: String,
+      item_total_ht: String,
+      item_total_ttc: String,
+      item_discount_percentate: String,
+      item_discount_amount: String,
+      item_vat_id: String,
+      item_vat_rate: String,
+      item_vat_amount: String,
+    },
+  ],
+  Reject_reason: String,
+  Total_ttc_amount: String,
+  Total_ht_amount: String,
+  Date_creation: String,
+  Cooking_time: String,
 });
 
-const Order = mongoose.model("Order", storeSchema);
+const Order = mongoose.model('Order', storeSchema);
 module.exports = { Order };
